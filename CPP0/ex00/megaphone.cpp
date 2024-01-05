2cf20b73ce6556480f0c52bba5f935087e896c6a
Aceauses <aceauses@Aceausess-MacBook-Pro.fritz.box>
2024-01-05 23:16:17 +0100

    CPP0/EX00 Done [first commit]

---
+++ CPP0/ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aceauses <aceauses@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 23:10:21 by aceauses          #+#    #+#             */
+/*   Updated: 2024/01/05 23:13:29 by aceauses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
